3393152b40566218ed835bb1d2114cb257a2ebfd
mohamed gueni <guenimystro@gmail.com>
2024-04-27 20:33:13 +0100

    hv filter

--- 0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/Model_Parameters.py
+++ 0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/Model_Parameters.py
@@ -93,29 +93,15 @@ PFC         =  {
                   'HS1'             :  PFC_SW,   
                   'HS2'             :  PFC_SW,   
                   'LS1'             :  PFC_SW,   
-                  'LS2'             :  PFC_SW,
-
-
+                  'LS2'             :  PFC_SW
                }	
 CTRL_PFC    =  {
-                  'modulator_gain'  : 2,  
-                  'Vref'    		   : 400,  
-                  'fs'    		      : 70e-3,  
-                  'Ri_F '           :  50e3,
-                  'Ri_Ts'           :  1/50e3,
-                  'Ri_Tp'           :  (1/50e3)/2.0,
-                  'Ri_Ti'           :  2/PFC_glb['Rbusin']*((1/50e3)/2.0),
-                  'Ri_Tn'           :  PFC_glb['L']/PFC_glb['Rbusin'],
-                  'Ri_Kp'           :  (PFC_glb['L']/PFC_glb['Rbusin'])/(2/PFC_glb['Rbusin']*((1/50e3)/2.0)),
-                  'Ri_Ki'           :  1/(2/PFC_glb['Rbusin']*((1/50e3)/2.0)),
-                  'Rv_F '           :  2e3,
-                  'Rv_Ts'           :  1/2e3,
-                  'Rv_Tp'           :  (1/2e3)/2*10,
-                  'Rv_Ti'           :  8/PFC_glb['Cout']['Cap_s']*((1/2e3)/2*10)*((1/2e3)/2*10),
-                  'Rv_Tn'           :  4*((1/2e3)/2*10),
-                  'Rv_Kp'           :  (4*((1/2e3)/2*10))/(8/PFC_glb['Cout']['Cap_s']*((1/2e3)/2*10)*((1/2e3)/2*10)),
-                  'Rv_Ki'           :  1/(8/PFC_glb['Cout']['Cap_s']*((1/2e3)/2*10)*((1/2e3)/2*10))
-
+                  'Vref'    		   :  400,  
+                  'fs'    		      :  70e-3,  
+                  'Ri_Kp'           :  0.1,
+                  'Ri_Ki'           :  600,
+                  'Rv_Kp'           :  5,
+                  'Rv_Ki'           :  800
                }
 DCLink      =  {
                   'Config'		      : 4,
@@ -125,99 +111,87 @@ DCLink      =  {
                   'nPara'		      : 6, 
                   'nSeri'		      : 1, 
                   'Vinit'		      : 0, 
-                  'Iinit'		      : 0, 
-
+                  'Iinit'		      : 0
                }
 Load        =  {
                   'Config'		      : 4,  
                   'CL'    		      : 0, 
-                  'RL'		         : (400*400)/7000,  
+                  'Vout'            :  400,  
+                  'RL'		         : 40,  
                   'LL'		         : 1e-9, 
                   'Vinit'		      : 0,  
-                  'Iinit'		      : 0, 
+                  'Iinit'		      : 0
                }
 LLC         =  {
-                  ## Sensing
-                  'R1'               : 4700/4,           # [Ohm] - sensing resistor
-                  'R2'               : 160/24,           # [Ohm] - sensing resistor
-                  'V_DC'          : 400,            # [V] - DC voltage source
-                  'n_prim'          : 10,            # [] - primary side turn number
-                  'n_sndry'         : 2,           # [] - secondary side turn number
-                  ## Primary side
-                  'L_r'               : 1.55e-6,   		  # [H] - resonant inductor
-                  'C_r'               : 1.2e-6,   		  # [F] - resonant capacitor
-                  ## Secondary side
-                  'L'               : 1.6e-6,   			  # [H] - output inductor
-                  'C_o'               : 480e-6,   		  # [F] - output capacitor
-                  'C_v_init'               : 0,   		  # [V] - output capacitor initial voltage
-                  ## Controller
-                  'sys_clk'               : 100e6,   	  # [Hz] - 100 MHz
-                  'max_period'               : 2000,     # Maximum system period allowed
-                  'min_period'               : 200,   	  # Minimum system period allowed
-                  ## Voltage change control
-                  'SlewStep'               : 50/3,     # [V/ms] - slew rate
-                  'StartUpInc'               : 500,      # [] - soft start frequency increment
-                
-                  'C_sense'               : 10e-9,       # [F] - sensing capacitor
-                  ## ADC conversion time
-                  'adc_conv'               : 15,         # [] - Number of system clock cycles required for ADC conversion
-                  ## Dead time
-                  'T_dt'               : 300e-9,         # [s] - dead time
-                  ## Thermal parameters 
-                  # 'mosfet'               : 'file:C2M0025120D',      # MOSFET thermal description
-                  'ron_mosfet'               : 30e-3,    	          # MOSFET on resistance (ohms)
-                  'vf_body_diode'               : 4.5,    	       # diode forward voltage (V)
-                  'ron_body_diode'               : 5e-3,            # diode on resistance (ohms)
-                  'rgon'               : 2.5,    				       # external turn-on gate resistance (ohms) 
-                  'rgoff'               : 2.5,   				       # external turn-off gate resistance (ohms) 
-                  'rth_ch'              : 0.1,   		 		       # thermal resistance case-heatsink (grease) (K/W)
-                  # 'diode'               : 'file:E4D20120G',   	# diode thermal description
-                  'ron_diode'               : 1,                # diode forward voltage (V)
-                  'vf_diode'               : 1.4,               # diode forward voltage (V) 
-                  'rth_ch_diode'              : 0.5,   		 	# thermal resistance case-heatsink (grease) (K/W)
-                  'num_par_diode'               : 4,            # Number of parallel diodes
-                  'Rth'               : 0.1,                    # Heatsink to ambient thermal resistance (K/W)
-                  't_init'               : 25                     					# initial temperature (C)
-             }
+                  'R1'              : 4700/4,          
+                  'R2'              : 160/24,          
+                  'V_DC'            : 400,          
+                  'n_prim'          : 10,            
+                  'n_sndry'         : 2,          
+                  'L_r'             : 1.55e-6,   		  
+                  'C_r'             : 1.2e-6,   		 
+                  'L'               : 1.6e-6,   			  
+                  'C_o'             : 480e-6,   		 
+                  'C_v_init'        : 0,   		  
+                  'sys_clk'         : 100e6,   	  
+                  'max_period'      : 2000,     
+                  'min_period'      : 200,   	 
+                  'SlewStep'        : 50/3,    
+                  'StartUpInc'      : 500,     
+                  'C_sense'         : 10e-9,       
+                  'adc_conv'        : 15,       
+                  'T_dt'            : 300e-9                          					
+               }
 Grid        =  {
                   'Config'          :  2,  
                   'Vin'             :  230,  
                   'Fgrid'           :  50,  
-                  'F'               :  50,  
-                  'Rg'              :  1e-2                                                                                  
-            }
+                  'Rg'              :  1e-3                                                                                  
+               }
 Thermals    =  {
                   'T_amb'           :  25.0,  
                   'rth_Amb'         :  0.09,  
+               }
+HV_Filter   =  {
+                  'Config'         :  1,
+                  'C_o'            : {                                                    
+                                          'Config'		      : 6,  
+                                          'Cap_s'    		   : 1e-3,  
+                                          'Resr_s'		      : 0,  
+                                          'Lesl_s'		      : 0,  
+                                          'Npara'		      : 1,  
+                                          'Nseri'		      : 1,  
+                                          'Vinit'		      : 0,  
+                                          'Iinit'		      : 0             
+                                          },
+                  'L'              :  40e-6
 
             }
-HV_Filter   =  {
-                  'Config'         :  2
-
+AC_Filter   =  {
+                  'Config'          :  1 ,
+                  'Cin'             :  1e-9,  
+                  'Lin'             :  1e-9,  
+                  'Ro'              :  1e-9, 
+                  'L_CMC'           :  1.5e-3, 
+                  'L_DMC'           :  900e-6, 
+                  'Cx'              :  0.1e-6,  
+                  'Cy1'             :  4.7e-12, 
+                  'Cy2'             :  4.7e-12,  
+                  'Cd'              :  0,  
+                  'Rd'              :  470e3
             }
 #?----------------------------------------------------------------------------------------------------------------------------------------
 ModelVars   =  {  'ToFile'          :  ToFile,
                   'scopes'          :  scopes,  
                   'Sim_param'       :  Sim_param,  
                   'Grid'            :  Grid,  
-                  'in_filter_config':  1, 
-                  'Cin'             :  1e-9,  
-                   'Lin'            :  1e-9,  
-                   'Ro'             :  1e-9, 
-                  'L_CMC'           :  (1.5e-3)/2, 
-                  'L_DMC'           :  (900e-6)/2, 
-                  'Cx'              :  1e-6,  
-                  'Cy1'             :  4.7e-9, 
-                  'Cy2'             :  4.7e-9,  
-                  'Cd'              :  100e-3,  
-                  'Rd'              :  470e3,  
+                  'AC_Filter'       :  AC_Filter,
                   'PFC_glb'         :  PFC_glb, 
                   'PFC'             :  PFC, 
                   'CTRL_PFC'        :  CTRL_PFC,
                   'DCLink'          :  DCLink,  
                   'LLC'             :  LLC, 
                   'HV_Filter'       :  HV_Filter,   
-                  'Vout'            :  400,  
                   'Load'            :  Load,
                   'Thermals'        :Thermals                                                                                   
                }	
