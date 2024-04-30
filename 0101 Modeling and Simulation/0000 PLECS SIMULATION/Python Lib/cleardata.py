a0b2ea46aa1298c4cca7e7a519980cd07c08093b
mohamed gueni <guenimystro@gmail.com>
2024-04-30 13:36:21 +0100

    auto clear data

--- 0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/cleardata.py
+++ 0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/cleardata.py
@@ -1,13 +1,21 @@
 
+#?----------------------------------------------------------------------------------------------------------------------------------------
+#?                                           ________                       __      __       
+#?                                          / ____/ /__  ____ ______   ____/ /___ _/ /_____ _
+#?                                         / /   / / _ \/ __ `/ ___/  / __  / __ `/ __/ __ `/
+#?                                        / /___/ /  __/ /_/ / /     / /_/ / /_/ / /_/ /_/ / 
+#?                                        \____/_/\___/\__,_/_/      \__,_/\__,_/\__/\__,_/
+#?
+#?----------------------------------------------------------------------------------------------------------------------------------------                                       
 import os
-
+#?----------------------------------------------------------------------------------------------------------------------------------------
 folder_paths = [
-(os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/CSV")).replace("\\", "/") ,
-(os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/html")).replace("\\", "/") ,
-(os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/Log")).replace("\\", "/") ,
-(os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/Traces")).replace("\\", "/") 
-
-]
+                (os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/CSV")).replace("\\", "/") ,
+                (os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/html")).replace("\\", "/") ,
+                (os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/Log")).replace("\\", "/") ,
+                (os.path.join(os.getcwd(), "0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/RES/Traces")).replace("\\", "/") 
+                ]
+#?----------------------------------------------------------------------------------------------------------------------------------------
 def delete_files_in_folder(folder_path):
     if not os.path.isdir(folder_path):
         print("Error: The provided path is not a directory.")
@@ -22,3 +30,4 @@ def delete_files_in_folder(folder_path):
 def clear_data_folders():
     for folder_path in folder_paths:
         delete_files_in_folder(folder_path)
+#?----------------------------------------------------------------------------------------------------------------------------------------
